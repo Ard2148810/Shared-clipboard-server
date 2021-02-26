e9e1218ac671d3fb647781410a3b2888fa74938c
Adrian Ałaszewski <wns2148810@interia.pl>
2021-02-26 09:27:25 +0100

    Clean up

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const WebSocket = require("ws");
 const shortid = require("shortid");
 
 const port = 5001;
-const wss = new WebSocket.Server({ port: port });
+const wss = new WebSocket.Server({ port });
 console.log(`Listenining on address:`);
 console.log(wss.address());
 
@@ -10,13 +10,11 @@ const INCORRECT_ROOM_ID = 4001;
 const INCORRECT_ROOM_ID_REASON = "Incorrect room ID";
 
 const rooms = new Map();
-let test;
 
 wss.on("connection", (ws, req) => {
     const wsRoomHeader = req.headers["sec-websocket-protocol"];
     if(!wsRoomHeader) {
         const roomId = createRoom();
-        test = roomId;
         rooms.get(roomId).add(ws);
         ws.roomId = roomId;
         sendRoomId(ws, roomId);
@@ -50,7 +48,7 @@ wss.on("connection", (ws, req) => {
     ws.on("close", () => {
         console.log("ws close")
         removeUserFromRoom(ws);
-    })
+    });
 });
 
 const broadcastMessage = (requester, message) => {
